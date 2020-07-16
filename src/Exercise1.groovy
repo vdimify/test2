c0160150c7535a07745893e973b34cb5292336f9
Viktoriya Dimitrova <viktoriyadd@gmail.com>
2020-07-16 19:12:25 +0300

    Test3,4,5

--- src/Exercise1.groovy
+++ src/Exercise1.groovy
@@ -1,13 +1,40 @@
 class Exercise1 {
 
-    static def isStringPalindrome = { String string ->
-        StringBuilder stringBuilder = new StringBuilder(string);
-        String reversedString = stringBuilder.reverse().toString()
-        return string == reversedString
+    def isStringPalindrome = { String string ->
+        def reverseString = reverseString(string)
+        return string == reverseString
     }
 
-    static def countWordsInSentence = {String string ->
+    def countWordsInSentence = { String string ->
         String[] words = string.split("\\s+");
         return words.size()
     }
+
+    def countOccurrencesOfLetter(String string, String let) {
+        def counter = 0
+        string.each { letter ->
+            if (letter == let)
+                counter++
+        }
+        return counter
+    }
+
+    def reverseString(def string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        String reversedString = stringBuilder.reverse().toString()
+        return reversedString
+    }
+
+    def switchLowerAndUpperCases(String s) {
+        char[] ch = s.toCharArray();
+        ch.eachWithIndex {letter, index->
+            if (Character.isLowerCase(letter)) {
+                ch[index] = letter.toUpperCase()
+            } else {
+                ch[index] = letter.toLowerCase()
+            }
+        }
+        return new String(ch)
+    }
+
 }
